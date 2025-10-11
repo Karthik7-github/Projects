e96ac1b45b058ea47937bce0a18736197b9595c6
Karthik Ponnam <Karthik@Ponnam.in>
2025-10-11 20:54:53 +0530

    Some changes

--- HTML AND CSS/Portofio/script.js
+++ HTML AND CSS/Portofio/script.js
@@ -3,7 +3,7 @@ let skillsection = document.querySelector('.skills-section');
 let boxes = document.querySelector('.boxes-bar2');
 let x = true;
 
-function more() {
+function moreski() {
   if (x) {
     skillsection.style.height = '140vh';
     boxes.classList.add('show');  // show boxes
@@ -14,4 +14,42 @@ function more() {
     x = true;
   }
 }
+
+
+
+
+let Achievmwntssection = document.querySelector('.Achievments');
+let boxess = document.querySelector('.boxes-notvisi');
+let y = true;
+
+function moreach() {
+  if (y) {
+    Achievmwntssection.style.height = '200vh';
+    boxess.classList.add('show');  // show boxes
+    y = false;
+  } else {
+    Achievmwntssection.style.height = '140vh';
+    boxess.classList.remove('show'); // hide boxes
+    y = true;
+  }
+}
+
+
+
+const allBoxes = document.querySelectorAll('.bb');
+
+allBoxes.forEach(item => {
+  const button = item.querySelector('.hoverBtn');
+  const box = item.querySelector('.box-in');
+
+  button.addEventListener('mouseenter', () => {
+    box.classList.add('glow');
+  });
+
+  button.addEventListener('mouseleave', () => {
+    box.classList.remove('glow');
+  });
+});
+
+
 
